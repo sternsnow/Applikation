cfe4e52e6ffe7570fdae2d4075a7dec6dbbc5c9c
sternsnow <Karl.bartholdson@gmail.com>
2025-05-13 16:43:21 +0200

    la till anstalld klass

---
+++ src/applikation/Anstalld.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package applikation;
+
+
+/**
+ *
+ * @author karlb
+ */
+public class Anstalld {
+
+    private String ePost;
+    private String losenord;
+    private String telefon;
+    private String adress;
+    private String behorighet;
+   
+
+    public Anstalld(){
+        
+    }
+    
+    
+    
+    public String getEpost()
+    {
+        return ePost;
+    }
+    
+    public void setEpost(String nyEpost)
+    {
+        this.ePost = nyEpost;
+    }
+    
+    public String getLosenord()
+    {
+        return losenord;
+    }
+    
+    public void setLosenord(String nyttLosenord)
+    {
+        this.losenord = nyttLosenord;
+    }
+    
+    public String getTelefon()
+    {
+        return telefon;
+    }
+    
+    public void setTelefon(String nyTelefon)
+    {
+        this.telefon = nyTelefon;
+    }
+            
+}
